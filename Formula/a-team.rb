16faaa17bfa5ed8f18073299f3d602116a29df30
James Crosswell <jamescrosswell@users.noreply.github.com>
2026-09-20 16:53:21 +1200

    a-team 0.0.4 (#10)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/a-team.rb
+++ Formula/a-team.rb
@@ -8,19 +8,19 @@ class ATeam < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/mentaldesk/a-team/releases/download/v0.0.3/a-team-0.0.3-osx-arm64.tar.gz"
-      sha256 "8e8f73f36ba0787006d612b8c9944a6c2d0d5d3ce738987700d2511250e17262"
+      url "https://github.com/mentaldesk/a-team/releases/download/v0.0.4/a-team-0.0.4-osx-arm64.tar.gz"
+      sha256 "6a78dca81f785645d925a27a45b0a5d6d820b3eaf1281ddcd10e1e6f7eaa1db1"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/mentaldesk/a-team/releases/download/v0.0.3/a-team-0.0.3-linux-x64.tar.gz"
-      sha256 "0744072edb4d5428ef4de06d1a2c3075f8ca4054ad560e7c08c14d77c9a93fde"
+      url "https://github.com/mentaldesk/a-team/releases/download/v0.0.4/a-team-0.0.4-linux-x64.tar.gz"
+      sha256 "8cae8d6f565f5937129526a2c4393ff934bc2c52ce699d3d6beed11332e38e52"
     end
     on_arm do
-      url "https://github.com/mentaldesk/a-team/releases/download/v0.0.3/a-team-0.0.3-linux-arm64.tar.gz"
-      sha256 "2acbc07e3a3f7b772158922253ecf4b28a19fcfd8bc3052791d27f74023942ff"
+      url "https://github.com/mentaldesk/a-team/releases/download/v0.0.4/a-team-0.0.4-linux-arm64.tar.gz"
+      sha256 "4a8c8c93c0d0220e0751c18de1f3de8f30864614fb36596c81ab7ff101ba77c2"
     end
   end
 
